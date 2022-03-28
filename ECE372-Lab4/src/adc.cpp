a1957f4575efa2e8cbda0f356642d17ae6431d6a
Cwbremser <90017447+Cwbremser@users.noreply.github.com>
2022-03-28 12:30:47 -0700

    Update adc.cpp
    ADC Complete. Please Review.

--- ECE372-Lab4/src/adc.cpp
+++ ECE372-Lab4/src/adc.cpp
@@ -9,4 +9,35 @@
  */
 void initADC(){
 
+    //Setting Reference Voltage to AVCC = 5V 
+    //In order to do this Setting ADMUX REFS1 And REFS0 to 01 respectively
+
+    ADMUX |= (1 << REFS0);
+    ADMUX &= ~( 1 << REFS1);
+
+    // Right Justified  for Admux!
+
+    ADMUX &= ~(1 << ADLAR);
+
+    // Auto Trigger Source Selection
+    // Free Running Mode. 
+    ADCSRB &= ~((1 << ADTS2) | (1 << ADTS1) | (1 << ADTS0));
+
+    // Enable ADC and Auto Triggering
+
+    ADCSRA |= ((1 << ADEN) | ( 1 << ADATE));
+
+    // ADC Clock Frequency. Prescaler of 128. 
+
+    ADCSRA |= ( 1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0)
+
+    // Disable ADC0 Pin Digital input 
+    //Pin is A0
+
+    DIDR0 |= (1 << ADC7D);
+
+    //Start ADC! 
+
+    ADCSRA |= (1 << ADSC);
+
 }
