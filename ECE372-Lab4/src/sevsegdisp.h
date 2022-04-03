255063ad0de130dc4a10faff7fb4c06d55f1d588
James Fulton <45501940+james-fulton@users.noreply.github.com>
2022-04-03 15:11:12 -0700

    Being Replaced

--- ECE372-Lab4/src/sevsegdisp.h
+++
@@ -1,9 +0,0 @@
-// Description: Function prototypes for sevsegdisp.cpp
-//----------------------------------------------------------------------//
-
-#ifndef SEVSEGDISP_H
-#define SEVSEGDISP_H
-
-void initSevSegDisp();
-
-#endif
