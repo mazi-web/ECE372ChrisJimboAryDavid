de821967d0fae885894e57476dcd38ebc6b68c54
Cwbremser <90017447+Cwbremser@users.noreply.github.com>
2022-04-11 16:06:24 -0700

    updating Spi

--- ECE372-LAB5/src/spi.h
+++ ECE372-LAB5/src/spi.h
@@ -1,8 +1,10 @@
 
-#ifndef SPI_H
-#define SPI_H
+#ifndef spi_h
+#define spi_h
 
 void SPI_MASTER_Init();
 void write_execute(unsigned char CMD, unsigned char data);
+void spi_smile_maker(unsigned char smile);
+
 
 #endif
