4b3f447477a26aee8041908191d21ee941e483d7
Ary Nath <44914593+DaRoastFTW@users.noreply.github.com>
2022-04-17 19:46:54 -0700

    Update switch.cpp

--- ECE372-LAB5/src/switch.cpp
+++ ECE372-LAB5/src/switch.cpp
@@ -12,15 +12,15 @@
  */
 void initSwitch(){
 //Set Data direction for input
-    DDRD &= ~(1 << PORTD0);
+    DDRJ &= ~(1 << PORTJ0);
 
     //Enable pullup resistor for stable reading between 0 and 1
-    PORTD |= (1 << PORTD0);
+    PORTJ |= (1 << PORTJ0);
 
     //Enable the pins for interrupt
     //attachInterrupt(digitalPinToInterrupt(PORTD0), ISR, CHANGE);
     //detachInterrupt(PORTD0)
-    EICRA |= (1 << ISC00);
-    EICRA &= ~(1 << ISC01);
-    EIMSK |= (1 << INT0);
+    EICRB |= (1 << ISC00);
+    EICRB &= ~(1 << ISC01);
+    EIMSK |= (1 << INT1);
 }
