9f54bc9be539e6703c8303ed99e94913cfe93f70
James Fulton <45501940+james-fulton@users.noreply.github.com>
2022-05-12 09:22:30 -0700

    Comments

--- ECE372FinalProject_Mk2/AD7920.h
+++ ECE372FinalProject_Mk2/AD7920.h
@@ -16,8 +16,7 @@
 #define DDR_SPI DDRB      // DDR on ATMEGA2560 for SPI is DDRB
 #define DD_SS DDB0        // SS Chip Select of ATMEGA2560 / CS            (Pin 53)
 //#define DD_SCK DDB1       // Clock pin on ATMEGA2560 / Clk                (Pin 52)
-//#define DD_MOSI DDB2      // MOSI on ATMEGA2560                           (Pin 51)
-//#define DD_MISO DDB3      // MOSI on ATMEGA2560                           (Pin 50)
+//#define DD_MISO DDB3      // MISO on ATMEGA2560                           (Pin 50)
 #define SPI_PORT PORTB    // Port for SPI on ATMEGA2560
 #define SPI_SS_BIT PORTB0 // Port Bit of Chip Select on ATMEGA2560
 
@@ -36,7 +35,7 @@ public:
 
         //dataIn |= SPI.transfer(0) << 8; //SPI.transfer16(0xffff);
         //dataIn |= SPI.transfer(0);
-        dataIn = SPI.transfer16(0);
+        dataIn = SPI.transfer16(0); // transfer 16 bits for the 12-bits sent from the ADC
 
         SPI_PORT |= (1 << SPI_SS_BIT); // SS Pin High
 
